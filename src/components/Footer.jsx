3a61a86280a9d6b4a8398bea0255a6778b8fdf50
manoj0357-ai <manojmanu035710@gmail.com>
2024-10-14 20:08:57 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>Copyrights&copy; 2024 Expense Tracker(Manu)</p>
+        </footer>
+    );
+}
+
+export default Footer;
